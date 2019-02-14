00d35e65d25f5438842fce357293e8996c051574
yangliu <861351494@qq.com>
2019-02-14 21:08:08 +0800

    登陆功能实现

--- src/main.js
+++ src/main.js
@@ -4,12 +4,27 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//引入css样式全局控制
+import './assets/css/global.css'
+
+//引入element-ui
+import ElementUI from 'element-ui'
+
+//引入图标库css样式文件
+import './assets/fonts/iconfont.css'
+//引入axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+// 给Vue注册全部的element-ui的标签组件 和 事件方法组件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
